5311a291de10697112e770f26f8074ab943ef8e3
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-03 14:53:30 -0700

    Loops

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           int start = 0;
+            int end = 5;
+            // loop from start to end including end
+            for (int i = start; i <= end; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // loop from start to end excluding end
+            for (int i = start; i < end; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //////////////////While///////////////////////
+            int j = 1;
+            while (j < 6)
+            {
+                Console.WriteLine(j);
+                j = j + 1;
+            }
+        }
+    }
+}
